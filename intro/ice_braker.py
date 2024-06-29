380b1dd67122175af5b205e29f6873d2a37346c0
venkatanagasai6 <venkatanagasai6@gmail.com>
2024-06-29 19:45:05 +0530

    initial load

---
+++ intro/ice_braker.py
@@ -0,0 +1,7 @@
+from langchain.prompts import PromptTemplate
+from langchain_openai import ChatOpenAI
+import os
+
+if __name__ == '__main__':
+    print('hello world..!')
+    print(os.environ['OPEN_AI_KEY'])
